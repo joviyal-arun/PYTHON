f9e1e9bb8f091106b9cc9480026a1058a27aaeed
joviyal-arun <joviyalarun07@gmail.com>
2023-07-17 22:00:05 +0530

    function_code

---
+++ FUNCTION.py
@@ -0,0 +1,17 @@
+# Default Parameter:-
+
+'''def add():
+    a=int(input("Enter Your Value:-"))
+    b=int(input("Enter Your Value:-"))
+    c=a+b
+    print(c)
+add()'''
+
+# Passing Argument:-
+
+def add(a,b):
+    c=a+b
+    print(c)
+a=int(input("Enter Your Value:-"))
+b=int(input("Enter Your Value:-"))
+add(a,b)
